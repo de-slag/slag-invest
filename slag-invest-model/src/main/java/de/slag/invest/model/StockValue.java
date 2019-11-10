833f98febb264d74646e10720dedbfd2abff6256
slipp <slipp@NB308.coinor.local>
2019-11-10 06:49:13 +0100

    init

---
+++ slag-invest-model/src/main/java/de/slag/invest/model/StockValue.java
@@ -0,0 +1,44 @@
+package de.slag.invest.model;
+
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+
+import de.slag.common.model.EntityBean;
+
+@Entity
+public class StockValue extends EntityBean {
+	
+	@Basic
+	private String isin;
+	
+	@Basic
+	private String name;
+	
+	@Basic
+	private String wkn;
+
+	public String getIsin() {
+		return isin;
+	}
+
+	public void setIsin(String isin) {
+		this.isin = isin;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getWkn() {
+		return wkn;
+	}
+
+	public void setWkn(String wkn) {
+		this.wkn = wkn;
+	}
+
+}
