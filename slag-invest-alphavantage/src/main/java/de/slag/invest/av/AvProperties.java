466d7b20b2220f126003647b768db3162c0cb0cd
slipp <slipp@NB308.coinor.local>
2019-11-10 22:21:16 +0100

    +property timeout

--- slag-invest-alphavantage/src/main/java/de/slag/invest/av/AvProperties.java
+++ slag-invest-alphavantage/src/main/java/de/slag/invest/av/AvProperties.java
@@ -4,5 +4,7 @@ public interface AvProperties {
 
 	String API_KEY = "de.slag.invest.av.apikey";
 	String OUTPUT_SIZE_FULL = "de.slag.invest.av.output.size.full";
+	String TIMEOUT = "de.slag.invest.av.timeout";
+	
 
 }
