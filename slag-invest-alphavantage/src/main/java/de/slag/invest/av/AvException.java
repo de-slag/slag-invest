bb372aeca9a72f5d29ed6b853ef1f6e52674812f
slipp <slipp@NB308.coinor.local>
2019-11-12 20:10:43 +0100

    +gson dependency, av calling needs this

--- slag-invest-alphavantage/src/main/java/de/slag/invest/av/AvException.java
+++ slag-invest-alphavantage/src/main/java/de/slag/invest/av/AvException.java
@@ -3,7 +3,7 @@ package de.slag.invest.av;
 public class AvException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
-
+	
 	public AvException(String s) {
 		super(s);
 	}
