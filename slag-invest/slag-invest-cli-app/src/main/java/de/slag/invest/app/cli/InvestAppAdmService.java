b8882bdb863c940d5bb7d050c07ab281c6c91e15
slipp <slipp@NB308.coinor.local>
2019-11-20 07:16:38 +0100

    invest app + service

--- slag-invest/slag-invest-cli-app/src/main/java/de/slag/invest/app/cli/InvestAppAdmService.java
+++ slag-invest/slag-invest-cli-app/src/main/java/de/slag/invest/app/cli/InvestAppAdmService.java
@@ -1,10 +1,17 @@
 package de.slag.invest.app.cli;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Properties;
+import java.util.Set;
 
 import javax.annotation.PostConstruct;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Service;
 
 import de.slag.common.base.AdmService;
@@ -12,16 +19,42 @@ import de.slag.common.base.BaseException;
 
 @Service
 public class InvestAppAdmService implements AdmService {
+	
+	private static final Log LOG = LogFactory.getLog(InvestAppAdmService.class);
+
+	public static final String CONFIG = "de.slag.configfile";
 
 	private Properties properties = new Properties();
 
 	@PostConstruct
 	public void setUp() {
-		final String configFilePath = System.getProperty("config");
+		final String configFilePath = System.getProperty(CONFIG);
 		if (StringUtils.isBlank(configFilePath)) {
 			throw new BaseException("property not setted: 'config'");
 		}
+		File file = new File(configFilePath);
+		if (!file.exists()) {
+			throw new BaseException("file not exists:" + configFilePath);
+		}
+		FileInputStream inStream;
+		try {
+			inStream = new FileInputStream(file);
+		} catch (FileNotFoundException e) {
+			throw new BaseException(e);
+		}
+		try {
+			properties.load(inStream);
+		} catch (IOException e) {
+			throw new BaseException(e);
+		}
 		
+		// TODO: debug log configured properties
+		
+		
+		
+		
+		LOG.info("setUp successful");
+
 	}
 
 	public String getValue(String key) {
