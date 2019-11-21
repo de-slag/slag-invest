45598e43ad8cd84ee7195c4c387473ed7e460bcc
slipp <slipp@NB308.coinor.local>
2019-11-21 07:16:44 +0100

    + Report service + impl

---
+++ slag-invest/slag-invest-report/src/main/java/de/slag/invest/appservice/report/ReportServiceImpl.java
@@ -0,0 +1,19 @@
+package de.slag.invest.appservice.report;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import de.slag.invest.service.StockValueService;
+
+@Service
+public class ReportServiceImpl implements ReportService {
+	
+	@Resource
+	private StockValueService stockValueService;
+
+	public String testReport() {
+		return "this is an empty test report";
+	}
+
+}
