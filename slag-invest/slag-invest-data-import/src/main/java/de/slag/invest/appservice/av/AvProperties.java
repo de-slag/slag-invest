6f24ab7f4cdf711461e8b8808471dd008a000db5
slipp <slipp@NB308.coinor.local>
2019-11-22 08:09:02 +0100

    + av-dependencies and connection-impls to fetch data from this interface

---
+++ slag-invest/slag-invest-data-import/src/main/java/de/slag/invest/appservice/av/AvProperties.java
@@ -0,0 +1,10 @@
+package de.slag.invest.appservice.av;
+
+public interface AvProperties {
+
+	String API_KEY = "de.slag.invest.av.apikey";
+	String OUTPUT_SIZE_FULL = "de.slag.invest.av.output.size.full";
+	String TIMEOUT = "de.slag.invest.av.timeout";
+	
+
+}
---
+++ slag-invest/slag-invest-data-import/src/main/java/de/slag/invest/appservice/av/stock/AvStockResponse.java
@@ -0,0 +1,9 @@
+package de.slag.invest.appservice.av.stock;
+
+import java.util.Collection;
+
+public interface AvStockResponse {
+	
+	Collection<AvStock> getStocks();
+
+}
