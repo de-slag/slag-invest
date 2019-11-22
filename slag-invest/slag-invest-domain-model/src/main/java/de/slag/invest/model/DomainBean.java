850de78fd204ae38fa3f590563bb215d3edcf665
sebastian <sebastian@phobos.speedport.ip>
2019-11-22 15:22:18 +0100

    ~DomainBean comparison

--- slag-invest/slag-invest-domain-model/src/main/java/de/slag/invest/model/DomainBean.java
+++ slag-invest/slag-invest-domain-model/src/main/java/de/slag/invest/model/DomainBean.java
@@ -1,7 +1,18 @@
 package de.slag.invest.model;
 
+import java.util.UUID;
+
+import javax.persistence.Transient;
+
 import de.slag.common.model.EntityBean;
 
-public class DomainBean extends EntityBean {
+public abstract class DomainBean extends EntityBean {
+
+	@Transient
+	private UUID uuid = UUID.randomUUID();
+
+	public UUID getUuid() {
+		return uuid;
+	}
 
 }
