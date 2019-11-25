3450656a66ce4beabc8435611769681a66888310
slipp <slipp@NB308.coinor.local>
2019-11-25 06:46:59 +0100

    +init file repo service

---
+++ slag-invest/slag-invest-file-repository/src/main/java/de/slag/invest/filerepo/PortfolioPositionDto.java
@@ -0,0 +1,15 @@
+package de.slag.invest.filerepo;
+
+public class PortfolioPositionDto {
+	
+	private String portfolioNumber;
+
+	public String getPortfolioNumber() {
+		return portfolioNumber;
+	}
+
+	public void setPortfolioNumber(String portfolioNumber) {
+		this.portfolioNumber = portfolioNumber;
+	}
+
+}
