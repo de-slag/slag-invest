2257332d137fc6ec63431b111b88817200abb3a9
slipp <slipp@NB308.coinor.local>
2020-01-23 19:57:16 +0100

    refactoring

--- slag-invest/slag-invest-cli-app/src/main/java/de/slag/invest/app/cli/InvestAppAdmCacheImpl.java
+++ slag-invest/slag-invest-cli-app/src/main/java/de/slag/invest/app/cli/InvestAppAdmCacheImpl.java
@@ -15,7 +15,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 import de.slag.common.base.AdmCache;
 import de.slag.common.base.BaseException;
