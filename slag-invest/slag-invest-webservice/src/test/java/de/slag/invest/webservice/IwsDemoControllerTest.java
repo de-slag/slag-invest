2cc4b344d1878f40945772e20baf76b9592b6bec
slipp <slipp@NB308.coinor.local>
2020-03-06 07:24:31 +0100

    refactoring

--- slag-invest/slag-invest-webservice/src/test/java/de/slag/invest/webservice/IwsDemoControllerTest.java
+++ slag-invest/slag-invest-webservice/src/test/java/de/slag/invest/webservice/IwsDemoControllerTest.java
@@ -14,19 +14,19 @@ class IwsDemoControllerTest {
 
 	private static final String BASE_URL = "http://localhost:18080/slag-invest-webservice/demo";
 	
-	private WebTarget demoBaseEndpoint;
-	private WebTarget demoResponseEndpoint;
+	private WebTarget baseEndpoint;
+	private WebTarget responseEndpoint;
 	private WebTarget dtoResponseEndpoint;
 
 	@BeforeEach
 	public void setUp() {
-		demoBaseEndpoint = TestWebTargetUtils.create(BASE_URL);
-		demoResponseEndpoint = TestWebTargetUtils.create(BASE_URL + "/response");
+		baseEndpoint = TestWebTargetUtils.create(BASE_URL);
+		responseEndpoint = TestWebTargetUtils.create(BASE_URL + "/response");
 		dtoResponseEndpoint = TestWebTargetUtils.create(BASE_URL + "/dto");
 	}
 	
 	@Test
-	void demoDtoResponseEndpointUsingResponse() {
+	void dtoResponseEndpointUsingResponseTest() {
 		final Response response = dtoResponseEndpoint.request().get();
 		assertNotNull(response);
 		assertEquals(200, response.getStatus());
@@ -37,7 +37,7 @@ class IwsDemoControllerTest {
 	}
 
 	@Test
-	void demoDtoResponseEndpoint() {
+	void dtoResponseEndpointTest() {
 		final DemoDto demoDto = dtoResponseEndpoint.request().get(DemoDto.class);
 		assertNotNull(demoDto);
 		assertNotNull(demoDto.getId());
@@ -45,15 +45,15 @@ class IwsDemoControllerTest {
 	}
 
 	@Test
-	void demoResponseEndpoint() {
-		final Response response = demoResponseEndpoint.request().get();
+	void responseEndpointTest() {
+		final Response response = responseEndpoint.request().get();
 		assertNotNull(response);
 		assertTrue(response.getStatus() == 200);
 	}
 
 	@Test
-	void demoBaseEndpoint() {
-		assertEquals("demo", demoBaseEndpoint.request().get(String.class));
+	void baseEndpointTest() {
+		assertEquals("demo", baseEndpoint.request().get(String.class));
 	}
 	
 
