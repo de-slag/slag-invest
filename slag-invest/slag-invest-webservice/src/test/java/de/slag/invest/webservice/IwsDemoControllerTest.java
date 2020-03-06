382a93b5a0a6d4f511ab73f9a36a718c68820e3a
slipp <slipp@NB308.coinor.local>
2020-03-06 12:20:37 +0100

    ION-17: /save + unit tests
    unit tests in numeric order

--- slag-invest/slag-invest-webservice/src/test/java/de/slag/invest/webservice/IwsDemoControllerTest.java
+++ slag-invest/slag-invest-webservice/src/test/java/de/slag/invest/webservice/IwsDemoControllerTest.java
@@ -1,33 +1,99 @@
 package de.slag.invest.webservice;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import de.slag.invest.webcommon.DemoDto;
 
+@TestMethodOrder(OrderAnnotation.class)
 class IwsDemoControllerTest {
 
 	private static final String BASE_URL = "http://localhost:18080/slag-invest-webservice/demo";
-	
+
 	private WebTarget baseEndpoint;
 	private WebTarget responseEndpoint;
-	private WebTarget dtoResponseEndpoint;
+	private WebTarget dtoEndpoint;
+
+	private WebTarget createEndpoint;
+
+	private WebTarget loadEndpoint;
+
+	private WebTarget saveEndpoint;
 
 	@BeforeEach
 	public void setUp() {
 		baseEndpoint = TestWebTargetUtils.create(BASE_URL);
 		responseEndpoint = TestWebTargetUtils.create(BASE_URL + "/response");
-		dtoResponseEndpoint = TestWebTargetUtils.create(BASE_URL + "/dto");
+		dtoEndpoint = TestWebTargetUtils.create(BASE_URL + "/dto");
+		createEndpoint = TestWebTargetUtils.create(BASE_URL + "/create");
+		loadEndpoint = TestWebTargetUtils.create(BASE_URL + "/load");
+		saveEndpoint = TestWebTargetUtils.create(BASE_URL + "/save");
 	}
-	
+
 	@Test
-	void dtoResponseEndpointUsingResponseTest() {
-		final Response response = dtoResponseEndpoint.request().get();
+	@Order(7)
+	void saveEndpointTest() {
+
+		final DemoDto demoDto = new DemoDto();
+		demoDto.setId(1L);
+		demoDto.setName("Demo Dto");
+
+		final Response response = saveEndpoint.request(MediaType.APPLICATION_JSON).accept(MediaType.TEXT_PLAIN_TYPE).post(Entity.json(demoDto));
+		assertEquals(200, response.getStatus());
+		
+		
+
+	}
+
+	@Test
+	@Order(6)
+	void simplePostTest() {
+		final Response response = TestWebTargetUtils.create(BASE_URL + "/test").request(MediaType.TEXT_PLAIN).accept(MediaType.TEXT_PLAIN)
+				.post(Entity.entity("demo test", MediaType.TEXT_PLAIN));
+		assertEquals(200, response.getStatus());
+		final String readEntity = response.readEntity(String.class);
+		assertNotNull(readEntity);
+		assertEquals("OK: demo test", readEntity);
+	}
+
+	@Test
+	@Order(5)
+	void loadEndpointTest() {
+		final Response response = loadEndpoint.queryParam("id", 1L).request().get();
+		assertNotNull(response);
+		assertEquals(200, response.getStatus());
+		final DemoDto demoDto = response.readEntity(DemoDto.class);
+		assertNotNull(demoDto);
+	}
+
+	@Test
+	@Order(4)
+	void createEndpointTest() {
+		final Response response = createEndpoint.request().get();
+		assertNotNull(response);
+		assertEquals(200, response.getStatus());
+		final String readEntity = response.readEntity(String.class);
+		assertNotNull(readEntity);
+		assertEquals(1L, Long.valueOf(readEntity));
+	}
+
+	@Test
+	@Order(3)
+	void dtoEndpointUsingResponseTest() {
+		final Response response = dtoEndpoint.request().get();
 		assertNotNull(response);
 		assertEquals(200, response.getStatus());
 		final DemoDto demoDto = response.readEntity(DemoDto.class);
@@ -37,14 +103,16 @@ class IwsDemoControllerTest {
 	}
 
 	@Test
-	void dtoResponseEndpointTest() {
-		final DemoDto demoDto = dtoResponseEndpoint.request().get(DemoDto.class);
+	@Order(2)
+	void dtoEndpointTest() {
+		final DemoDto demoDto = dtoEndpoint.request().get(DemoDto.class);
 		assertNotNull(demoDto);
 		assertNotNull(demoDto.getId());
 		assertNotNull(demoDto.getName());
 	}
 
 	@Test
+	@Order(1)
 	void responseEndpointTest() {
 		final Response response = responseEndpoint.request().get();
 		assertNotNull(response);
@@ -52,9 +120,8 @@ class IwsDemoControllerTest {
 	}
 
 	@Test
+	@Order(0)
 	void baseEndpointTest() {
 		assertEquals("demo", baseEndpoint.request().get(String.class));
 	}
-	
-
 }
