ea469912102e12e6461e0b8f3d7455ef3a4f3490
slipp <slipp@NB308.coinor.local>
2020-03-10 06:31:03 +0100

    IONE-17: config controller +tests

--- slag-invest/slag-invest-webservice/src/main/java/de/slag/invest/webservice/crud/AbstractIwsCrudController.java
+++ slag-invest/slag-invest-webservice/src/main/java/de/slag/invest/webservice/crud/AbstractIwsCrudController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import de.slag.invest.model.Mandant;
 import de.slag.invest.webservice.CredentialToken;
 import de.slag.invest.webservice.IwsCredentialComponent;
 
@@ -18,17 +19,17 @@ public abstract class AbstractIwsCrudController<T> {
 	@Resource
 	private IwsCredentialComponent iwsCredentialComponent;
 
-	protected abstract Long create0();
+	protected abstract Long create0(Mandant mandant);
 
-	protected abstract T load0(long id);
+	protected abstract T load0(long id, Mandant mandant);
 
-	protected abstract void save0(T t);
+	protected abstract void save0(T t, Mandant mandant);
 
-	protected abstract void delete0(long id);
+	protected abstract void delete0(long id, Mandant mandant);
 
 	protected void validate(String token) {
 		try {
-			if (!iwsCredentialComponent.isValid(CredentialToken.of(token))) {
+			if (!isValid(CredentialToken.of(token))) {
 				throw new IllegalAccessException("credentials not valid");
 			}
 		} catch (IllegalAccessException e) {
@@ -36,30 +37,38 @@ public abstract class AbstractIwsCrudController<T> {
 		}
 	}
 
+	private boolean isValid(final CredentialToken of) {
+		return iwsCredentialComponent.isValid(of);
+	}
+
+	protected Mandant getMandant(String token) {
+		return iwsCredentialComponent.getMandant(CredentialToken.of(token));
+	}
+
 	@GetMapping("/load")
 	public T load(@RequestParam("id") long id, @RequestParam(value = "token", required = false) String token) {
 		validate(token);
-		return load0(id);
+		return load0(id, getMandant(token));
 	}
 
 	@PostMapping(path = "/save", consumes = MediaType.APPLICATION_JSON)
 	public Response save(@RequestBody T t, @RequestParam(value = "token", required = false) String token) {
 		validate(token);
-		save0(t);
+		save0(t, getMandant(token));
 		return Response.ok().build();
 	}
 
 	@DeleteMapping(path = "/delete")
 	public Response delete(@RequestParam("id") long id, @RequestParam(value = "token", required = false) String token) {
 		validate(token);
-		delete0(id);
+		delete0(id, getMandant(token));
 		return Response.ok().build();
 	}
 
 	@GetMapping("/create")
 	public Long create(@RequestParam(value = "token", required = false) String token) {
 		validate(token);
-		return create0();
+		return create0(getMandant(token));
 	}
 
 }
