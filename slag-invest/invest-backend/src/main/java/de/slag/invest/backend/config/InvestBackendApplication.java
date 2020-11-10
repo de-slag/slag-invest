d959e5a7cac81bc17fb440775cf8090bb0a4c653
post@sebastianlipp.de <post@sebastianlipp.de>
2020-11-10 12:31:16 +0100

    init scheduling

--- slag-invest/invest-backend/src/main/java/de/slag/invest/backend/config/InvestBackendApplication.java
+++ slag-invest/invest-backend/src/main/java/de/slag/invest/backend/config/InvestBackendApplication.java
@@ -2,8 +2,10 @@ package de.slag.invest.backend.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = { "de.slag" })
+@EnableScheduling
 public class InvestBackendApplication {
 
 	public static void main(String[] args) {
