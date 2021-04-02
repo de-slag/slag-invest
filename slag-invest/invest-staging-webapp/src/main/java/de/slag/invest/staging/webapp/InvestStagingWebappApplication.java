a8ad7e2686797cfa252fa66873c6fefb584616c1
post@sebastianlipp.de <post@sebastianlipp.de>
2021-04-02 15:04:35 +0200

    STA-002: init webapp project

---
+++ slag-invest/invest-staging-webapp/src/main/java/de/slag/invest/staging/webapp/InvestStagingWebappApplication.java
@@ -0,0 +1,13 @@
+package de.slag.invest.staging.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestStagingWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestStagingWebappApplication.class, args);
+	}
+
+}
---
+++ slag-invest/invest-staging-webapp/src/test/java/de/slag/invest/staging/webapp/InvestStagingWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package de.slag.invest.staging.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestStagingWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
